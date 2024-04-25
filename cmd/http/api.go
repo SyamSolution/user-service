521a1b931edb4965a8e171241986d0d964ecdbee
syamsularie <syams.arie@gmail.com>
2024-04-25 14:12:41 +0700

    setup logger

--- cmd/http/api.go
+++ cmd/http/api.go
@@ -2,11 +2,17 @@ package main
 
 import (
 	"fmt"
+	"github.com/SyamSolution/user-service/config"
 	"github.com/gofiber/fiber/v2"
+	"github.com/joho/godotenv"
 	"log"
+	"os"
 )
 
 func main() {
+	baseDep := config.NewBaseDep()
+	loadEnv(baseDep.Logger)
+
 	app := fiber.New()
 
 	app.Get("/", func(c *fiber.Ctx) error {
@@ -16,5 +22,15 @@ func main() {
 	//=== listen port ===//
 	if err := app.Listen(fmt.Sprintf(":%s", "3000")); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func loadEnv(logger config.Logger) {
+	_, err := os.Stat(".env")
+	if err == nil {
+		err = godotenv.Load()
+		if err != nil {
+			logger.Error("no .env files provided")
+		}
 	}
 }
