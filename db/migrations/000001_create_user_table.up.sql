842db2c9680c0407d509e905fac0d0d17bded908
syamsularie <syams.arie@gmail.com>
2024-05-01 16:16:58 +0700

    add migration

---
+++ db/migrations/000001_create_user_table.up.sql
@@ -0,0 +1,14 @@
+CREATE TABLE IF NOT EXISTS user (
+    user_id INT PRIMARY KEY AUTO_INCREMENT,
+    username VARCHAR(50) NOT NULL,
+    email VARCHAR(100) NOT NULL,
+    password VARCHAR(100) NOT NULL,
+    full_name VARCHAR(100),
+    phone_number VARCHAR(20),
+    address VARCHAR(255),
+    city VARCHAR(100),
+    country VARCHAR(100),
+    postal_code VARCHAR(20),
+    nik VARCHAR(20), -- National Identification Number
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
