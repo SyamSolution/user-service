75151f19ff37a37193c624488f02b9520401a4e2
Syamsul Bachri <ea_dev@telkomsel.co.id>
2024-05-19 16:43:49 +0800

    syamsul/update-table-user

--- db/migrations/000001_create_user_table.up.sql
+++ db/migrations/000001_create_user_table.up.sql
@@ -2,7 +2,6 @@ CREATE TABLE IF NOT EXISTS user (
     user_id INT PRIMARY KEY AUTO_INCREMENT,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
-    password VARCHAR(100) NOT NULL,
     full_name VARCHAR(100),
     phone_number VARCHAR(20),
     address VARCHAR(255),
