fe53b9abb6d527750ead23a5fa1b11c18f0133ed
Syamsul Bachri <ea_dev@telkomsel.co.id>
2024-06-08 09:30:55 +0800

    syamsul/update-logger

--- cmd/http/api.go
+++ cmd/http/api.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/SyamSolution/user-service/config"
 	"github.com/SyamSolution/user-service/config/middleware"
@@ -11,6 +12,10 @@ import (
 	"github.com/SyamSolution/user-service/internal/repository"
 	"github.com/SyamSolution/user-service/internal/usecase"
 	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+	"github.com/gofiber/fiber/v2/middleware/logger"
+	"github.com/gofiber/fiber/v2/middleware/pprof"
+	"github.com/gofiber/fiber/v2/middleware/recover"
 	"github.com/joho/godotenv"
 	"github.com/prometheus/client_golang/prometheus"
 )
@@ -46,6 +51,19 @@ func main() {
 	//=== handler lists end ===//
 
 	app := fiber.New()
+
+
+	app.Use(recover.New())
+	app.Use(cors.New())
+	app.Use(pprof.New())
+	app.Use(logger.New(logger.Config{
+		// Format: "[${time}] ${status} - ${latency} ${method} ${path}\n",
+		Format:       `${time} {"router_activity" : [${status},"${latency}","${method}","${path}"], "query_param":${queryParams}, "body_param":${body}}` + "\n",
+		TimeInterval: time.Millisecond,
+		TimeFormat:   "02-01-2006 15:04:05",
+		TimeZone:     "Indonesia/Jakarta",
+	}))
+
 	
 	app.Get("/", func(c *fiber.Ctx) error {
 		return c.SendString("UP")
